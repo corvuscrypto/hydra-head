ae1d16d3e872df2358e586f7e631eec7cbc31ed2
Clifford Richardson <cmrallen@gmail.com>
2017-02-26 13:20:11 -0800

    Added some code to init the connection to the master

--- connection.go
+++ connection.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crypto/cipher"
 	"encoding/gob"
+	"log"
 	"net"
 )
 
@@ -24,8 +25,39 @@ func (e encryptedConnection) Read(dst []byte) (n int, err error) {
 	return
 }
 
+func newMasterConn(t *net.TCPConn) (conn *encryptedConnection, err error) {
+	conn = new(encryptedConnection)
+	conn.tcpConn = t
+	//create a new private key for exchange
+	priv, X, Y, err := createNewKey()
+	if err != nil {
+		return nil, err
+	}
+	//TODO: Write key exchange
+	conn.cipherBlock, err = createNewCipher(priv, X, Y)
+	return
+}
+
 type masterConnection struct {
 	conn    *encryptedConnection
 	encoder *gob.Encoder
 	decoder *gob.Decoder
+}
+
+var globalConnection *masterConnection
+
+func connectToMaster() {
+
+	masterAddr, err := net.ResolveTCPAddr("tcp", globalConfig.Master.MasterAddress+":"+globalConfig.Master.MasterPort)
+	if err != nil {
+		log.Fatal("Unable to resolve address for master:", err)
+	}
+
+	tcpConn, err := net.DialTCP("tcp", nil, masterAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//make the new encrypted connection
+	newMasterConn(tcpConn)
 }
