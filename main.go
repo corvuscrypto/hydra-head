6f68bc22520a2d9be49fc0e10479379ce3d56cc0
Clifford Richardson <cmrallen@gmail.com>
2017-02-26 14:57:51 -0800

    Added ID generation at startup

--- main.go
+++ main.go
@@ -1,6 +1,23 @@
 package main
 
+import (
+	"crypto/rand"
+	"fmt"
+)
+
+var ID string
+
+func createID() {
+	//generate just random data for now
+	buffer := make([]byte, 16)
+	rand.Read(buffer)
+	ID = fmt.Sprintf("%x", buffer)
+}
+
 func main() {
+	createID()
+	fmt.Println(ID)
 	//run the connection routine
 	connectToMaster()
+	select {}
 }
