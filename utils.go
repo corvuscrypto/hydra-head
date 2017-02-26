d0025600ebcd832f1e24643e406452ee6813f86e
Clifford Richardson <cmrallen@gmail.com>
2017-02-26 12:59:11 -0800

    copied over the utils for packet ID creation from the master repo

---
+++ utils.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/gob"
+	"sync/atomic"
+	"time"
+)
+
+type packetID uint64
+
+var packetPerturbator uint64
+
+func newPacketID() packetID {
+	perturbator := atomic.AddUint64(&packetPerturbator, uint64(1)) % 1000
+	now := time.Now()
+	micro := uint64(now.Nanosecond()/1000) * 1000
+	timestamp := uint64(now.Unix()*1000000000) + micro
+	return packetID(timestamp + perturbator)
+}
+
+func receivePacket(d *gob.Decoder, expectedPacket interface{}) error {
+	return d.Decode(expectedPacket)
+}
+
+func sendPacket(e *gob.Encoder, packet interface{}) {
+	e.Encode(packet)
+}
